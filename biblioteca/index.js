c3bb3c16ecfd03c581bc64964d02fd8c5f5e6a0e
Alejandro <alejoub1998@gmail.com>
2023-08-27 14:13:33 -0500

    Se agrega en el archivo index.js la funcionalidad e cambiar el texto cuando pase el mouse

--- biblioteca/index.js
+++ biblioteca/index.js
@@ -3,3 +3,11 @@ document.getElementById("cambiarColorBtn").addEventListener("click", function ()
     var colorHex = '#' + Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = colorHex;
 });
+
+document.getElementById("textoCambiante").addEventListener("mouseover", function () {
+    this.textContent = "¡El texto cambió!";
+});
+
+document.getElementById("textoCambiante").addEventListener("mouseout", function () {
+    this.textContent = "Pasa el mouse aquí para cambiar el texto.";
+});
